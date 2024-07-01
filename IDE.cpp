a138948a97a8dcd088c2ea5290f93e2af46c262b
Aryan-chaudhry <Aryanchaudhary2629@gmail.com>
2024-07-01 21:46:44 +0530

    comit

--- IDE.cpp
+++ IDE.cpp
@@ -1,33 +1,19 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 
-void reverse(char arr[], int size){
-
-    int st = 0;
-    int end = size-1;
-    while(st <= end){
-        swap(arr[st], arr[end]);
-        st++;
-        end--;
-    }
-}
-
-void print(char arr[], int size){
-    for(int i=0; i<size; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-
-
 int main(){
-    char arr[] = {'h','e','l','l','o'};
-    int size = 5;
-    cout << "Before reverse : ";
-    print(arr,5);
-    reverse(arr, 5);
-    cout << "After reverse : ";
-    print(arr, 5);
-    return 0;
+    string s = "egg", t = "add";
+
+    unordered_map<char, char>mp;
+
+    // see mapping
+    for(int i=0; i<s.size(); i++){
+        mp[s[i]] = t[i];
+    }
+    // traverse
+    for(auto i:mp){
+        cout << i.first << " " << i.second << endl;
+    }
+
 }
---
+++ Pointers/intro.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int a = 5;
+    // int* ptr = &a;
+    // cout << sizeof(ptr) << endl;
+
+    // char ch = 'a';
+    // char* ptr2 = &ch;
+    // cout << sizeof(ptr2) << endl;
+
+    // long l = 1040;
+    // long* ptr3 = &l;
+    // cout << sizeof(ptr3) << endl;
+
+    // cout << a << endl; // 5
+    // cout << &a << endl; // address of a 
+    // cout << ptr << endl; // address of a
+    // cout << &ptr << endl; // address of ptr
+    // cout << *ptr << endl;
+
+    int *ptr;
+    cout << *ptr << endl;
+    
+}
